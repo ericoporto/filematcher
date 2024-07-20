22a27a4e7fd71908e9105614ad4234d91c94fce9
e <ericoporto2008@gmail.com>
2024-07-19 21:27:23 -0300

    initial version of getting a list of all files

--- main.cpp
+++ main.cpp
@@ -1,5 +1,86 @@
 #include <iostream>
 #include <string>
+#include <regex>
+#include <functional>
+
+#if defined(_WIN32)
+// desktop win32 software
+#include "Windows.h"
+
+
+std::pair<std::vector<std::string>,std::vector<std::string>> get_files_and_dirs(const std::string &dirname) {
+    std::pair<std::vector<std::string>,std::vector<std::string>> result;
+    std::vector<std::string> files;
+    std::vector<std::string> dirs;
+    std::string path = dirname;
+
+    if(dirname.back() == '/' || dirname.back() == '\\') {
+        path = dirname + "*";
+    } else if(dirname.back() != '*' && dirname.back() != '/') {
+        path = dirname + "\\*";
+    }
+
+    HANDLE fileHandle;
+    WIN32_FIND_DATA ffd;
+    LARGE_INTEGER szDir;
+    WIN32_FIND_DATA fileData;
+    fileHandle = FindFirstFile(path.c_str(), &ffd);
+
+    if (INVALID_HANDLE_VALUE == fileHandle) {
+        printf("Invalid File Handle Value \n");
+        return result;
+    }
+
+    do
+    {
+        std::string f = ffd.cFileName;
+        bool is_valid_name = f != "." && f != "..";
+        bool is_dir = ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY;
+        if(!is_valid_name)
+            continue;
+        if(is_dir)
+        {
+            dirs.emplace_back(f);
+        }
+        else
+        {
+            files.emplace_back(f);
+        }
+    } while (FindNextFile(fileHandle, &ffd) != 0);
+
+    result.first = files;
+    result.second = dirs;
+
+    return result;
+}
+
+std::vector<std::string> get_all_files(const std::string &dirname)
+{
+    std::vector<std::string> allfiles;
+    std::vector<std::string> stack;
+    stack.push_back(dirname);
+
+    while (!stack.empty()) {
+        std::string current_dir = stack.back();
+        stack.pop_back();
+
+        auto result = get_files_and_dirs(current_dir);
+        auto files = result.first;
+        auto dirs = result.second;
+
+        for (const auto &file : files) {
+            allfiles.push_back(current_dir + "\\" + file);
+        }
+
+        for (const auto &dir : dirs) {
+            stack.push_back(current_dir + "\\" + dir);
+        }
+    }
+
+    return allfiles;
+}
+
+#endif
 
 
 
@@ -7,6 +88,13 @@
 int main(int argc, char *argv[]) {
     std::string path = ".";
     if (argc > 1) { path = argv[1]; }
+
+    std::vector<std::string> files = get_all_files(path);
+
+    for(std::string file : files)
+    {
+        printf("%s\n", file.c_str());
+    }
 
     std::cout << "Reads 'filematch.txt' and outputs all files from directory that matches description or nothing if no 'filematch.txt' is found." << std::endl;
     return 0;
